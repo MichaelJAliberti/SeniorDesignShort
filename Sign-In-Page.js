45e9f19e70ad81ad0cd92ce4830d18e13c52b5cb
MichaelJAliberti <mjali@bu.edu>
2021-09-16 01:23:22 -0400

    enabled navigation from longin page

--- Sign-In-Page.js
+++ Sign-In-Page.js
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
-import { 
-    Text, 
-    SafeAreaView, 
-    TextInput, 
-    StyleSheet,
-    Button } 
-from "react-native";
+import React from "react";
+import { SafeAreaView, StyleSheet, Button } from "react-native";
 import * as Google from 'expo-google-app-auth';
 import * as firebase from 'firebase';
 
 export default function SignInPage({ navigation: { navigate } }) {
-    const [text, onChangeText] = React.useState("Useless Text");
-    const onPressLogin = () => {
-        navigate('Recipes')
-    }
 
     return (
         <SafeAreaView style={styles.container}>
             <Button
-                title="SIGN-IN WITH GOOGLE"
+                title='SIGN-IN WITH GOOGLE'
                 style={styles.button}
-                onPress={() => signInWithGoogleAsync()}
+                onPress={
+                    async () => {
+                        var result = await signInWithGoogleAsync();
+                        if (result.cancelled || result.error){
+                            alert('Login failed');
+                        } else {
+                            navigate('Recipes');
+                        }
+                    }
+                }
             />
         </SafeAreaView>
     )
@@ -35,6 +34,7 @@ async function signInWithGoogleAsync() {
         });
   
         if (result.type === 'success') {
+            onSignIn(result)
             return result.accessToken;
         } else {
             return { cancelled: true };
@@ -42,6 +42,50 @@ async function signInWithGoogleAsync() {
     } catch (e) {
         return { error: true };
     }
+}
+
+function onSignIn(googleUser) {
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!isUserEqual(googleUser, firebaseUser)) {
+            // Build Firebase credential with the Google ID token.
+            var credential = firebase.auth.GoogleAuthProvider.credential(
+                googleUser.idToken,
+                googleUser.accessToken
+            );
+  
+            // Sign in with credential from the Google user.
+            firebase.auth().signInWithCredential(credential).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+        } else {
+            console.log('User already signed-in Firebase.');
+        }
+    });
+  }
+  
+
+function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+        var providerData = firebaseUser.providerData;
+        for (let i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === googleUser.user.id) {
+                    // We don't need to reauth the Firebase connection.
+                    return true;
+                }
+        }
+    }
+    return false;
 }
 
 const styles = StyleSheet.create ({
