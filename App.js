3d5aa6f2c3d8b92b83711367517cc1ec0e4432b4
MichaelJAliberti <mjali@bu.edu>
2021-09-16 00:06:50 -0400

    removed excess imports, added authentication

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInPage from './Sign-In-Page'
@@ -20,12 +18,3 @@ export default function App() {
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-});
--- Sign-In-Page.js
+++ Sign-In-Page.js
@@ -1,42 +1,47 @@
 import React, { Component } from "react";
 import { 
-    View, 
     Text, 
     SafeAreaView, 
     TextInput, 
     StyleSheet,
-    Button } from "react-native";
-import RecipeHistoryPage from './Recipe-History-Page'
+    Button } 
+from "react-native";
+import * as Google from 'expo-google-app-auth';
+import * as firebase from 'firebase';
 
 export default function SignInPage({ navigation: { navigate } }) {
     const [text, onChangeText] = React.useState("Useless Text");
-    const [number, onChangeNumber] = React.useState(null);
     const onPressLogin = () => {
         navigate('Recipes')
     }
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Login</Text>
-            <Text>Email</Text>
-            <TextInput 
-                style={styles.input}
-                placeholder="e.g. youremail@gmail.com"
-                onChangeText={onChangeText}
-            /> 
-            <Text>Password</Text>
-            <TextInput 
-                style={styles.input}
-                placeholder="Type your password"
-                onChangeText={onChangeText}
-            /> 
             <Button
-                title="LOGIN"
+                title="SIGN-IN WITH GOOGLE"
                 style={styles.button}
-                onPress={onPressLogin}
+                onPress={() => signInWithGoogleAsync()}
             />
         </SafeAreaView>
     )
+}
+
+async function signInWithGoogleAsync() {
+    try {
+        const result = await Google.logInAsync({
+            androidClientId: '107579648655-8qk5bk9o15662og8h0rc9k61a66hdebj.apps.googleusercontent.com',
+            iosClientId: '107579648655-uc1vct2lhfdd1obv711u4dk8dbg78brk.apps.googleusercontent.com+',
+            scopes: ['profile', 'email'],
+        });
+  
+        if (result.type === 'success') {
+            return result.accessToken;
+        } else {
+            return { cancelled: true };
+        }
+    } catch (e) {
+        return { error: true };
+    }
 }
 
 const styles = StyleSheet.create ({
